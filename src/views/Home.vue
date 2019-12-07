c4a5a45e653826e11be27d61a4133bf91a5aafcd
lmh <lmh862974698@gmail.com>
2019-12-07 21:49:17 +0800

    修改样式，将fixed定位全部修改为flex布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,23 +9,23 @@
       <div class="navList" ref="container">
         <van-tabs v-model="active" v-show="flag" animated>
           <van-tab title="新歌">
-            <NewMusic  @to-parent="getMusicId"></NewMusic>
+            <NewMusic @to-parent="getMusicId"></NewMusic>
           </van-tab>
           <van-tab title="排行">
-            <RankingList  @to-parent="getMusicId"></RankingList>
+            <RankingList @to-parent="getMusicId"></RankingList>
           </van-tab>
           <van-tab title="歌单">
-            <SongList  @to-parent="getMusicId"></SongList>
+            <SongList @to-parent="getMusicId"></SongList>
           </van-tab>
           <van-tab title="歌手">
-            <Singer  @to-parent="getMusicId"></Singer>
+            <Singer @to-parent="getMusicId"></Singer>
           </van-tab>
         </van-tabs>
         <van-nav-bar v-show="!flag" title="搜索" left-text left-arrow @click-left="index" />
+        <Search v-show="!flag" @to-parent="getMusicId"></Search>
       </div>
-      <Search v-show="!flag"  @to-parent="getMusicId"></Search>
     </div>
-     <listening v-if="listenFlag" :key="getId" :musicId="getId"></listening>
+    <listening v-if="listenFlag" :key="getId" :musicId="getId"></listening>
   </div>
 </template>
 
@@ -37,15 +37,14 @@ import SongList from "./SongList.vue";
 import Singer from "./Singer.vue";
 import Search from "./Search.vue";
 
-
 export default {
   data() {
     return {
       container: null,
       active: 0,
       flag: true,
-      getId:'',
-      listenFlag:false
+      getId: "",
+      listenFlag: false
     };
   },
   mounted() {
@@ -67,13 +66,15 @@ export default {
     index() {
       this.flag = true;
       this.active = 0;
-    },getMusicId(i){
-      this.getId=i
-      this.listenFlag=true;
+    },
+    getMusicId(i) {
+      this.getId = i;
+      this.listenFlag = true;
       setTimeout(() => {
         document.getElementById("listening").style.height = 0;
         document.getElementById("top").style.height = 0;
-        document.getElementsByClassName("iptSearch")[0].style.paddingBottom = "65px";
+        document.getElementsByClassName("iptSearch")[0].style.paddingBottom =
+          "65px";
       });
     }
   }
@@ -85,24 +86,26 @@ export default {
   display: flex;
   flex-direction: column;
 }
-#header {
-  height: 46px;
-}
+
 #content {
   flex: 1;
   overflow: auto;
 }
+.navList {
+  overflow: auto;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
 .van-tabs[data-v-fae5bece] {
-  position: relative;
-  padding-top: 44px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
 .search {
   font-size: 20px;
 }
-.van-nav-bar{position:fixed;width:100%;}
-.iptSearch{padding-top:44px;}
-.hotSearch{padding-top:54px;}
 
 .van-sticky--fixed {
   background: #fff;
